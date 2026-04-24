b7c95d7ff7dbbf841598eed95ff5c29febecc6b1
Lewis Dukelow <35412216+m2a9x45@users.noreply.github.com>
2026-04-24 19:21:11 +0100

    v0.0.3

--- Formula/certy.rb
+++ Formula/certy.rb
@@ -1,14 +1,14 @@
 class Certy < Formula
   desc "CLI tool to work with eIDAS QWACs & QSEALs"
   homepage "https://github.com/m2a9x45/certy"
-  version "0.0.2"
+  version "0.0.3"
 
   if Hardware::CPU.arm?
-    url "https://binaries.certy.lewisdukelow.com/v0.0.2/certy_0.0.2_darwin_arm64.tar.gz"
-    sha256 "def1a80019506526ffbd044f7b1390943adfd19eedfdba8e44c693176a7b38dc"
+    url "https://binaries.certy.lewisdukelow.com/v0.0.3/certy_0.0.3_darwin_arm64.tar.gz"
+    sha256 "6977655b11eb042988c66c5ed79bdc144b61288580b23687ddbdf8d3cf4dd73b"
   else
-    url "https://binaries.certy.lewisdukelow.com/v0.0.2/certy_0.0.2_darwin_amd64.tar.gz"
-    sha256 "cdaf68cfa80d64bdd54c71b8a0b940b83ca37079b13a34afcce284450c9399fa"
+    url "https://binaries.certy.lewisdukelow.com/v0.0.3/certy_0.0.3_darwin_amd64.tar.gz"
+    sha256 "7de40b3816d2f9a60d2bf212f8846e07c2cd8fd748e99635b6ab10a85f882dcf"
   end
 
   def install
